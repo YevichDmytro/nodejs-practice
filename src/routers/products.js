39c838db86e71ce936d4f7b6a5792b366dbc4933
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-09-07 10:35:16 +0200

    finish

--- src/routers/products.js
+++ src/routers/products.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
-import { getAllProductsController } from '../controllers/products.js';
+import {
+  getAllProductsController,
+  getProductsControllerById,
+} from '../controllers/products.js';
 
 const router = Router();
 
 router.get('/products', ctrlWrapper(getAllProductsController));
+
+router.get('/products/:productId', ctrlWrapper(getProductsControllerById));
 
 export default router;
